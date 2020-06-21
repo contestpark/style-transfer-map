a4bf8728c56e97e54e2cf0410ad470cf3d932c91
contestpark <contestpark@naver.com>
2020-06-21 17:47:27 +0900

    finish_real

--- app/src/main/java/edu/skku/map/changer/LoginActivity.java
+++ app/src/main/java/edu/skku/map/changer/LoginActivity.java
@@ -1,8 +1,11 @@
 package edu.skku.map.changer;
 
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -46,7 +49,11 @@ public class LoginActivity extends AppCompatActivity
     private FirebaseAuth mAuth;
     private GoogleApiClient mGoogleApiClient;
     private static final int RC_SIGN_IN = 1000;
-
+    String[] permission_list = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.INTERNET
+    };
 
     private ProgressBar mProgressBar;
 
@@ -55,6 +62,7 @@ public class LoginActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        checkPermission();
         mProgressBar = findViewById(R.id.progressBar_l);
         button = findViewById(R.id.google_sign_in_button);
         mAuth = FirebaseAuth.getInstance();
@@ -76,6 +84,39 @@ public class LoginActivity extends AppCompatActivity
         });
 
         setupFirebaseAuth();
+    }
+
+    private void checkPermission() {
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return;
+
+        for(String permission : permission_list){
+            //권한 허용 여부를 확인한다.
+            int chk = checkCallingOrSelfPermission(permission);
+
+            if(chk == PackageManager.PERMISSION_DENIED){
+                //권한 허용을여부를 확인하는 창을 띄운다
+                requestPermissions(permission_list,0);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==0)
+        {
+            for(int i=0; i<grantResults.length; i++)
+            {
+                //허용됬다면
+                if(grantResults[i]==PackageManager.PERMISSION_GRANTED){
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"앱권한설정하세요",Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            }
+        }
     }
 
     private void showDialog(){
